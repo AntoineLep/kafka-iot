05fe84de9c119fe77fb339ac83d22732786254ad
Antoine Leprevost <leprevost.antoine@gmail.com>
2017-11-23 22:14:03 +0100

    simplify config file declaration

--- system_launcher/config/SystemConfig.py
+++ system_launcher/config/SystemConfig.py
@@ -5,6 +5,13 @@ class SystemConfig(ConfigFile):
 
     _SYSTEM_CONFIG = {
         "LOG_LEVEL": "DEBUG",
+        "SYSTEM_LAUNCHER": {
+            "PATH": "~/PycharmProjects/kafka-iot/system_launcher",
+            "APP_NAME": "System Launcher",
+            "GROUP_ID": "system-launcher",
+            "LOG_DIRECTORY": "logs",
+            "VERSION": "1.0",
+        },
         "ZOOKEEPER": {
             "HOST": "localhost",
             "PORT": 2181
@@ -18,50 +25,44 @@ class SystemConfig(ConfigFile):
                     "PORT": "9092"
                 }
             ],
+            # ----------------- #
+            # TOPIC DECLARATION
+            # ----------------- #
             "TOPIC_LIST": [
                 {"NAME": "ping", "REPLICATION_FACTOR": 1, "PARTITION_NUMBER": 10},
-                {"NAME": "event-generation-request", "REPLICATION_FACTOR": 1, "PARTITION_NUMBER": 10},
                 {"NAME": "log", "REPLICATION_FACTOR": 1, "PARTITION_NUMBER": 10}
+            ],
+            # ------------------- #
+            # GLOBAL KAFKA RIGHTS
+            # ------------------- #
+            "INTERNAL_APP_INHERITANCE": {
+                "KAFKA_RIGHTS": {
+                    "READ": ["ping"],
+                    "WRITE": ["ping, log"]
+                }
+            },
+            # ---------------------------------------- #
+            # GROUP ID LIST WITH SPECIFIC KAFKA RIGHTS
+            # ---------------------------------------- #
+            "GROUP_ID_LIST": [
+                {
+                    "NAME": "system-launcher",
+                    "KAFKA_RIGHTS": {
+                        "READ": ["*"],
+                        "WRITE": ["*"],
+                    }
+                },
+                {
+                    "NAME": "app-1",
+                    "KAFKA_RIGHTS": {
+                        "READ": ["toto"],
+                        "WRITE": ["titi"],
+                    }
+                }
             ]
         },
-        "SYSTEM_LAUNCHER": {
-            "PATH": "~/PycharmProjects/kafka-iot/system_launcher",
-            "APP_NAME": "System Launcher",
-            "LOG_DIRECTORY": "logs",
-            "VERSION": "1.0",
-            "KAFKA_RIGHTS": {
-                "READ": ["ping"],
-                "WRITE": ["ping"],
-                "GROUP_ID": "system_launcher"
-            }
-        },
-        "INTERNAL_APP_INHERITANCE": {
-            "KAFKA_RIGHTS": {
-                "READ": ["ping"],
-                "WRITE": ["ping"]
-            }
-        },
-        "EVENT_GENERATOR": {
-            "NAME": "EventGenerator",
-            "TYPE": "internal",
-            "USER": "EventGenerator",
-            "PASSWORD": "EventGenerator_password",
-            "KAFKA_RIGHTS": {
-                "READ": ["event_generation_request"],
-                "WRITE": ["*"]
-            }
-        },
-        "APPS": [  # USED BY THE EVENT GENERATOR
-            {
-                "NAME": "App1",
-                "TYPE": "internal",
-                "USER": "App1",
-                "PASSWORD": "app1_password",
-                "KAFKA_RIGHTS": {
-                    "READ": ["topic_1", "topic_2"],
-                    "WRITE": ["event_generation_request", "log"]
-                }
-            }
+        "APP_LIST": [
+            {"NAME": "App1", "GROUP_ID": "app-1"}
         ]
     }
 
