2d9b07fad99e7dc283c395ac0034dc42c95d5f05
Antoine Leprevost <leprevost.antoine@gmail.com>
2017-11-30 00:07:27 +0100

    Config file format and coherence check + app list loading

--- system_launcher/Utils.py
+++ system_launcher/Utils.py
@@ -1,4 +1,5 @@
 import logging
+from common.KafkaIotException import KafkaIotException
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -11,3 +12,22 @@ def log_console_output(title, output_msg):
     :title output_msg: str
     """
     logging.debug("Console output >>> %s >>> %s" % (title, output_msg))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def check_kafka_right_in_topic_name_list(kafka_rights, topic_name_list):
+    """
+    Check kafka right topics exists in topic list
+    :param kafka_rights: Dictionary with read and write topic list
+    :type kafka_rights: dict
+    :param topic_name_list: List of topic name
+    :type topic_name_list: list[str]
+    :return: True if all the topics in kafka rights are in topic name list. Raise an exception otherwise
+    """
+    for read_topic in kafka_rights["READ"]:
+        if read_topic != "*" and read_topic not in topic_name_list:
+            raise KafkaIotException("READ topic \"%s\" is not declared in topic list" % read_topic)
+
+    for write_topic in kafka_rights["WRITE"]:
+        if write_topic != "*" and write_topic not in topic_name_list:
+            raise KafkaIotException("WRITE topic \"%s\" is not declared in topic list" % write_topic)
--- system_launcher/main.py
+++ system_launcher/main.py
@@ -10,6 +10,8 @@ from common.KafkaIotException import KafkaIotException
 from common.entities.kafka.Topic import Topic
 from common.entities.kafka.KafkaRights import KafkaRights
 from common.entities.kafka.GroupId import GroupId
+from common.entities.Application import Application
+from system_launcher.Utils import check_kafka_right_in_topic_name_list
 
 
 if __name__ == '__main__':
@@ -20,11 +22,77 @@ if __name__ == '__main__':
     log_level = sys_conf.get("LOG_LEVEL")
     log_location = os.path.join(sys_conf.get("SYSTEM_LAUNCHER.PATH"), sys_conf.get("SYSTEM_LAUNCHER.LOG_DIRECTORY"))
     app_name = sys_conf.get("SYSTEM_LAUNCHER.APP_NAME")
+    app_version = sys_conf.get("SYSTEM_LAUNCHER.VERSION")
     init_logger(log_level, log_location, app_name)
 
     logging.info("------------------------------")
-    logging.info("Starting %s" % app_name)
+    logging.info("Starting %s V%s" % (app_name, app_version))
     logging.info("------------------------------")
+
+    # ---------------------------------- #
+    # CONFIGURATION FILE FORMAT CHECKING
+    # ---------------------------------- #
+
+    logging.info("Checking configuration file format...")
+
+    check_fields_in_dict(sys_conf.get("ZOOKEEPER"), ["HOST", "PORT"], "ZOOKEEPER")
+    check_fields_in_dict(sys_conf.get("KAFKA"), ["PATH", "BROKER_LIST", "TOPIC_LIST", "KAFKA_RIGHTS_INHERITANCE",
+                                                 "GROUP_ID_LIST"], "KAFKA")
+    for broker in sys_conf.get("KAFKA.BROKER_LIST"):
+        check_fields_in_dict(broker, ["ID", "HOST", "PORT"], "KAFKA.BROKER_LIST")
+
+    for topic in sys_conf.get("KAFKA.TOPIC_LIST"):
+        check_fields_in_dict(topic, ["NAME", "REPLICATION_FACTOR", "PARTITION_NUMBER"], "KAFKA.TOPIC_LIST")
+
+    check_fields_in_dict(sys_conf.get("KAFKA.KAFKA_RIGHTS_INHERITANCE"), ["READ", "WRITE"],
+                         "KAFKA.KAFKA_RIGHTS_INHERITANCE")
+
+    for group_id in sys_conf.get("KAFKA.GROUP_ID_LIST"):
+        check_fields_in_dict(group_id, ["NAME", "KAFKA_RIGHTS"], "KAFKA.GROUP_ID_LIST")
+        check_fields_in_dict(group_id["KAFKA_RIGHTS"], ["READ", "WRITE"], "KAFKA.GROUP_ID_LIST.{NAME=%s}"
+                             % group_id["NAME"])
+
+    for app in sys_conf.get("APP_LIST"):
+        check_fields_in_dict(app, ["NAME", "GROUP_ID"], "APP_LIST")
+
+    logging.info("Configuration file format checking done!")
+
+    # ------------------------------------- #
+    # CONFIGURATION FILE COHERENCE CHECKING
+    # ------------------------------------- #
+
+    logging.info("Checking configuration file coherence...")
+
+    # check topics appear once
+    config_topic_name_list = [config_topic["NAME"] for config_topic in sys_conf.get("KAFKA.TOPIC_LIST")]
+    if len(set(config_topic_name_list)) != len(config_topic_name_list):
+        raise KafkaIotException("One or several topic(s) in topic list is(are) declared several times")
+
+    # check group ids appear once
+    config_group_id_name_list = [config_group_id["NAME"] for config_group_id in sys_conf.get("KAFKA.GROUP_ID_LIST")]
+    if len(set(config_group_id_name_list)) != len(config_group_id_name_list):
+        raise KafkaIotException("One or several group id(s) in group id list is(are) declared several times")
+
+    # check applications appear once
+    config_app_name_list = [config_app["NAME"] for config_app in sys_conf.get("APP_LIST")]
+    if len(set(config_app_name_list)) != len(config_app_name_list):
+        raise KafkaIotException("One or several app(s) in app list is(are) declared several times")
+
+    # Check kafka rights use existing topics
+    check_kafka_right_in_topic_name_list(sys_conf.get("KAFKA.KAFKA_RIGHTS_INHERITANCE"), config_topic_name_list)
+
+    for config_group_id in sys_conf.get("KAFKA.GROUP_ID_LIST"):
+        check_kafka_right_in_topic_name_list(config_group_id["KAFKA_RIGHTS"], config_topic_name_list)
+
+    # Check applications use existing group id
+    config_group_id_name_list = [config_group_id["NAME"] for config_group_id in sys_conf.get("KAFKA.GROUP_ID_LIST")]
+
+    for config_app in sys_conf.get("APP_LIST"):
+        if config_app["GROUP_ID"] not in config_group_id_name_list:
+            raise KafkaIotException("Group id \"%s\" used in app \"%s\" is not declared in group id list"
+                                    % (config_app["GROUP_ID"], config_app["NAME"]))
+
+    logging.info("Configuration file coherence checking done!")
 
     # ------------ #
     # KAFKA DRIVER
@@ -45,8 +113,6 @@ if __name__ == '__main__':
 
     failed_topic_creation = 0
     for topic_desc in sys_conf.get("KAFKA.TOPIC_LIST"):
-        check_fields_in_dict(topic_desc, ["NAME", "REPLICATION_FACTOR", "PARTITION_NUMBER"], "KAFKA.TOPIC_LIST")
-
         if topic_desc["REPLICATION_FACTOR"] <= len(kafka_broker_list):
             try:
                 kafka.create_topic(topic_desc["NAME"], topic_desc["REPLICATION_FACTOR"], topic_desc["PARTITION_NUMBER"])
@@ -70,7 +136,7 @@ if __name__ == '__main__':
         existing_topics.append(kafka.describe_topic(topic_name))
 
     logging.info("%d topic(s) created!" % (len(existing_topics) - failed_topic_creation))
-    logging.info("Available topics are: [%s]" % ", ".join([topic.name for topic in existing_topics]))
+    logging.info("Available topic(s) is(are): [%s]" % ", ".join([topic.name for topic in existing_topics]))
 
     # ---------------- #
     # TOPIC ALTERATION
@@ -118,41 +184,37 @@ if __name__ == '__main__':
     # ----------------- #
 
     logging.info("Initializing kafka rights management...")
-    conf_rights_inheritance = sys_conf.get("KAFKA.KAFKA_RIGHTS_INHERITANCE")
+    config_rights_inheritance = sys_conf.get("KAFKA.KAFKA_RIGHTS_INHERITANCE")
     rights_inheritance = None
-    conf_group_id_list = sys_conf.get("KAFKA.GROUP_ID_LIST")
+    config_group_id_list = sys_conf.get("KAFKA.GROUP_ID_LIST")
     group_id_list = []
-    conf_app_list = sys_conf.get("APP_LIST")
+    config_app_list = sys_conf.get("APP_LIST")
     app_list = []
 
-    check_fields_in_dict(conf_rights_inheritance, ["READ", "WRITE"], "KAFKA.KAFKA_RIGHTS_INHERITANCE")
-    rights_inheritance = KafkaRights(conf_rights_inheritance["READ"], conf_rights_inheritance["WRITE"])
+    rights_inheritance = KafkaRights(config_rights_inheritance["READ"], config_rights_inheritance["WRITE"])
 
     logging.info("Reading group id configuration...")
 
-    for c_group_id in conf_group_id_list:
-        check_fields_in_dict(c_group_id, ["NAME", "KAFKA_RIGHTS"], "KAFKA.GROUP_ID_LIST")
+    for c_group_id in config_group_id_list:
+        group_id_rights = copy.deepcopy(rights_inheritance)
+        group_id_rights.extend(KafkaRights(c_group_id["KAFKA_RIGHTS"]["READ"], c_group_id["KAFKA_RIGHTS"]["WRITE"]))
+        group_id = GroupId(c_group_id["NAME"], group_id_rights)
+        group_id_list.append(group_id)
 
-        if not any(g_id.name == c_group_id["NAME"] for g_id in group_id_list):
-            check_fields_in_dict(c_group_id["KAFKA_RIGHTS"], ["READ", "WRITE"], "KAFKA.GROUP_ID_LIST.{%s}"
-                                 % c_group_id["NAME"])
-            group_id_rights = copy.deepcopy(rights_inheritance)
-            group_id_rights.extend(KafkaRights(c_group_id["KAFKA_RIGHTS"]["READ"], c_group_id["KAFKA_RIGHTS"]["WRITE"]))
-            group_id = GroupId(c_group_id["NAME"], group_id_rights)
-            group_id_list.append(group_id)
-            logging.info("Group id %s loaded" % str(group_id))
-        else:
-            raise KafkaIotException("Group id with name \"%s\" is declared several times" % c_group_id["NAME"])
-
+    logging.info("Loaded group id(s) is(are): [%s]" % ", ".join([g_id.name for g_id in group_id_list]))
     logging.info("Group id configuration reading done!")
 
     logging.info("Reading application list...")
-    # TODO
+
+    for c_app in config_app_list:
+        app_group_id = [group_id for group_id in group_id_list if group_id.name == c_app["GROUP_ID"]][0]
+        app_list.append(Application(c_app["NAME"], app_group_id))
+
+    logging.info("Loaded app(s) is(are): [%s]" % ", ".join([app.name for app in app_list]))
     logging.info("Application list reading done!")
 
     logging.info("Applying rights to group id...")
     # TODO
-    # check group id read and write topic exist
     logging.info("Group id rights application done!")
 
     logging.info("Kafka rights management initialization done!")
